7839b661064b54ae98393ba7921d5fd02dfd1bcb
alper <30712287+alptunga1@users.noreply.github.com>
2018-10-16 23:32:45 +0300

    Update and rename deneme4.m to vsm.m

--- vsm.m
+++ vsm.m
@@ -6,7 +6,4 @@ figure
 imshow('area.png')
 [x,y] = getpts
 
-%her karenýn koordinatýný belirle . x bitene kadar if dönüsüne sok.
-%aralýklara göre bolgeler belirlensin. sayýsý yazýlsýn.
 % https://www.mathworks.com/help/images/ref/impoint.html
-
