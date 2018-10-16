a8891eb16f6be5046146714ef753eb34b76bc3e2
alper <30712287+alptunga1@users.noreply.github.com>
2018-10-16 23:28:07 +0300

    Add files via upload

---
+++ deneme4.m
@@ -0,0 +1,12 @@
+clc
+clear
+clear all
+
+figure
+imshow('area.png')
+[x,y] = getpts
+
+%her karenýn koordinatýný belirle . x bitene kadar if dönüsüne sok.
+%aralýklara göre bolgeler belirlensin. sayýsý yazýlsýn.
+% https://www.mathworks.com/help/images/ref/impoint.html
+
